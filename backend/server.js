92f8a7ba82cb4e1aac1b369d877b3f6feb7f6d74
unknown <wendel-vinicius@live.com>
2025-03-14 22:15:29 -0300

    Iniciando estrutura do backend e frontend

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API do Wizard de Carta de VAN rodando...");
+});
+
+app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
---
+++ frontend/index.js
@@ -0,0 +1,1 @@
+console.log("Frontend Wizard de Carta de VAN iniciado...");
