59cc326555bb6dad7e88cc1d206d9d0ca22f3a7a
KethellinPereira <kethellinpereira2018@outlook.com>
2025-05-12 20:18:30 -0300

    Ajuste de estrutura

--- src/cnabs/cnabs.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CnabsService } from './cnabs.service';
-import { CnabsController } from './cnabs.controller';
-
-@Module({
-  controllers: [CnabsController],
-  providers: [CnabsService],
-})
-export class CnabsModule {}
