dce0c6fd354fd82e5636672c7579970ea4351684
KethellinPereira <kethellinpereira2018@outlook.com>
2025-06-19 20:52:43 -0300

    Ajustando dependencias

--- backend/src/authorization-letters/authorization-letters.controller.ts
+++ backend/src/authorization-letters/authorization-letters.controller.ts
@@ -1,8 +1,6 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { AuthorizationService } from './authorization-letters.service';
 import { CreateAuthorizationLettersDto } from './dto/create-authorization-letters.dto';
-import { CreateAuthorizationLettersProductsDto } from './dto/create-authorization-letters-products.dto';
-import { CreateAuthorizationLettersVanTypesDto } from './dto/create-authorization-letters-van-types.dto';
 
 @Controller('authorization')
 export class AuthorizationController {
@@ -12,7 +10,7 @@ export class AuthorizationController {
   async saveAuthorizationLetters(
     @Body() createAuthorizationLettersDto: CreateAuthorizationLettersDto,
   ) {
-    return await this.authService.saveAuthorizationLetters(createFormAuthorizationLettersDto);
+    return await this.authService.saveAuthorizationLetters(createAuthorizationLettersDto);
   }
 
   @Get('authorization-letters')
