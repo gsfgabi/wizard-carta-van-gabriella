687bab3e8f45d105b17bda4989f93d1b298172bd
KethellinPereira <kethellinpereira2018@outlook.com>
2025-06-25 15:11:47 -0300

    Ajustando retorno do login

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -22,10 +22,33 @@ export class AuthService {
     return user.token === token;
   }
 
+  // async login(cnpj: string) {
+  //   const payload = { cnpj };
+  //   return {
+  //     access_token: this.jwtService.sign(payload),
+  //   };
+  // }
+
   async login(cnpj: string) {
-    const payload = { cnpj };
+    const user = await this.prisma.users.findUnique({
+      where: { cnpj },
+      select: {
+        name: true,
+        company_name: true,
+        url_logo: true,
+      },
+    });
+  
+    if (!user) {
+      throw new Error('Usuário não encontrado');
+    }
+  
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: this.jwtService.sign(cnpj),
+      name: user.name,
+      name_company: user.company_name,
+      url_logo: user.url_logo,
     };
   }
+  
 }
